b62b5ac7753815f333d1631655abc7698245c3d9
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-20 09:30:33 -0400

    editing readme

--- src/Deck.js
+++ src/Deck.js
@@ -11,6 +11,7 @@ export default class Deck extends Component {
     this.state = { deck: null, drawn: [], remainingCards: 52 };
 
     this.getCard = this.getCard.bind(this);
+    this.resetCards = this.resetCards.bind(this);
   }
 
   async componentDidMount() {
@@ -50,17 +51,32 @@ export default class Deck extends Component {
     }
   }
 
+  async resetCards() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+    this.setState({ deck: deck.data, drawn: [], remainingCards: 52 });
+  }
+
   render() {
     const cards = this.state.drawn.map(c => (
       <Card name={c.name} image={c.image} key={c.code} />
     ));
     return (
-      <div>
-        <h1>Deck Dealer</h1>
-        <button onClick={this.getCard}>Get Card!</button>
-        <p>Remaining Cards: {this.state.remainingCards}</p>
+      <div className="Deck">
+        <h1 className="Deck-title">&diams; Deck Dealer &clubs;</h1>
+        <h2 className="Deck-title subtitle">
+          &spades; Made with React by Kien &hearts;
+        </h2>
+        <button className="Deck-btn" onClick={this.getCard}>
+          DRAW CARD
+        </button>
+        <p className="Deck-remaining">
+          Remaining Cards: {this.state.remainingCards}
+        </p>
         <div className="Deck-cardarea">{cards}</div>
         <div className="Deck-remainingCards"></div>
+        <button className="Deck-reset-btn" onClick={this.resetCards}>
+          Reset
+        </button>
       </div>
     );
   }
